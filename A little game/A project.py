175e2d10c0e5fd4fed7b968880df904f12007ff9
CarlPettersson <54657589+CarlPettersson@users.noreply.github.com>
2020-03-20 12:51:19 +0100

    made the game endless
    changed spawn location. made it so the enemy get's faster the further you get. changed pause so it has proper names.

--- A little game/A project.py
+++ A little game/A project.py
@@ -56,7 +56,7 @@ class player(object):
         # pygame.draw.rect(display, blue, self.hitbox, 2)
 
     def hit(self):
-        self.x = 300
+        self.x = 600
         self.y = 350
         self.walk_count = 0
         self.jump = False
@@ -67,7 +67,7 @@ class player(object):
         display.blit(text, (display_width/2 - (text.get_width()/2), display_height/2))
         pygame.display.update()
         i = 0
-        while i < 100:
+        while i < 200:
             pygame.time.delay(10)
             i += 1
             for event in pygame.event.get():
@@ -139,7 +139,10 @@ class enemy(object):
         if self.health > 0:
             self.health -= 1
         else:
-            self.visible = False
+            # self.visible = False
+            self.health = 9
+            self.x = 0
+            self.velocity = 3 + score/5
         # print('hit')
 
 black = (0, 0, 0)                   #defining Colours
@@ -229,9 +232,77 @@ def game_intro():
         pygame.display.update()
         clock.tick(15)
 
+def pause():
+
+    pause = True
+
+    while pause:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        # print(click)
+
+        display.fill(white)
+        pause_font = pygame.font.SysFont('arial', 100)
+        text = pause_font.render('Game is paused', 1, black)
+        display.blit(text, (display_width/2 - (text.get_width()/2), display_height/2 - text.get_height()))          #shadow for the blue text
+
+        pause_font = pygame.font.SysFont('arial', 100)
+        text = pause_font.render('Game is paused', 1, (0, 0, 200))
+        display.blit(text, (display_width/2 - (text.get_width()/2) - 4, display_height/2 - text.get_height() - 4))  #blue text
+
+
+        if display_width/2 - 300 < mouse [0] and display_width/2 - 100 > mouse [0] and display_height/2 + 50 < mouse [1] and display_height/2 + 150 > mouse [1]:
+            pygame.draw.rect(display, green, (display_width/2 - 296, display_height/2 + 54, 200, 100))
+
+            green_button_text = pygame.font.SysFont('arial', 55)
+            text = green_button_text.render('Continue', 1, black)
+            display.blit(text, (display_width/2 - 291, display_height/2 + 69))
+            if click[0] == 1:
+                run = True
+                pause = False
+                   
+        else:
+            pygame.draw.rect(display, (0, 150, 0), (display_width/2 - 296, display_height/2 + 54, 200, 100))        #green button
+            pygame.draw.rect(display, (0, 150, 0), (display_width/2 - 297, display_height/2 + 53, 200, 100))
+            pygame.draw.rect(display, (0, 150, 0), (display_width/2 - 298, display_height/2 + 52, 200, 100))
+            pygame.draw.rect(display, (0, 150, 0), (display_width/2 - 299, display_height/2 + 51, 200, 100))
+            pygame.draw.rect(display, green, (display_width/2 - 300, display_height/2 + 50, 200, 100))
+
+            green_button_text = pygame.font.SysFont('arial', 55)
+            text = green_button_text.render('Continue', 1, black)
+            display.blit(text, (display_width/2 - 295, display_height/2 + 65))
+
+        if display_width/2 + 100 < mouse [0] and display_width/2 + 300 > mouse [0] and display_height/2 + 50 < mouse [1] and display_height/2 + 150 > mouse [1]:
+            pygame.draw.rect(display, red, (display_width/2 + 96, display_height/2 + 54, 200, 100))
+
+            red_button_text = pygame.font.SysFont('arial', 50)
+            text = red_button_text.render('Exit game', 1, black)
+            display.blit(text, (display_width/2 + 106, display_height/2 + 74))
+            if click[0] == 1:
+                pygame.quit()
+                quit()
+        else:
+            pygame.draw.rect(display, (150, 0, 0), (display_width/2 + 96, display_height/2 + 54, 200, 100))        # red button
+            pygame.draw.rect(display, (150, 0, 0), (display_width/2 + 97, display_height/2 + 53, 200, 100))
+            pygame.draw.rect(display, (150, 0, 0), (display_width/2 + 98, display_height/2 + 52, 200, 100))
+            pygame.draw.rect(display, (150, 0, 0), (display_width/2 + 99, display_height/2 + 51, 200, 100))
+            pygame.draw.rect(display, red, (display_width/2 + 100, display_height/2 + 50, 200, 100))
+
+            red_button_text = pygame.font.SysFont('arial', 50)
+            text = red_button_text.render('Exit game', 1, black)
+            display.blit(text, (display_width/2 + 110, display_height/2 + 70))
+
+        pygame.display.update()
+        clock.tick(15)
+
 #mainloop
 font = pygame.font.SysFont("arial", 30, True, True)
-character = player(300, 350, 64, 64)            #character size and starter position
+character = player(600, 350, 64, 64)            #character size and starter position
 Enemy = enemy(0, 350, 64, 64, 836)         #enemy path, size and starter position
 fire_rate = 0
 bullets = []            
@@ -248,10 +319,11 @@ while run:
     if Enemy.visible == True:
         if character.hitbox[1] < Enemy.hitbox[1] + Enemy.hitbox[3] and character.hitbox[1] + character.hitbox[3] > Enemy.hitbox[1]:
             if character.hitbox[0] + character.hitbox[2] > Enemy.hitbox[0] and character.hitbox[0] < Enemy.hitbox[0] + Enemy.hitbox[2]:
-                character.hit()
                 score = 0
+                Enemy.velocity = 3
                 Enemy.health = 9
                 Enemy.x = 0
+                character.hit()
 
     if fire_rate > 0:
         fire_rate += 1
@@ -322,7 +394,7 @@ while run:
             character.jump_height = 10
     
     if keys[pygame.K_ESCAPE]:
-        game_intro()
+        pause()
     
     UpdateDisplay()
 
